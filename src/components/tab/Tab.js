894e9a861efc2bb974d5f0fd4d8275253b66727d
nehasaipangallu <52908091+nehasaipangallu@users.noreply.github.com>
2022-12-07 13:41:35 +0530

    tab style improvements

--- src/components/tab/Tab.js
+++ src/components/tab/Tab.js
@@ -4,21 +4,24 @@ import Tabs from 'react-bootstrap/Tabs';
 import ActiveUserChart from '../active-user-chart/ActiveUserChart';
 import RevenueChart from '../revenue-chart/RevenueChart';
 import AppDownloads from '../appdownloads/AppDownloads';
+import './Tab.css';
 
 export default function Tab() {
   return (
-    <Tabs
-      defaultActiveKey="overview"
-      id="uncontrolled-tab-example"
-      className="mb-3"
-    >
-      <Tab eventKey="overview" title="Overview">
-        <ActiveUserChart></ActiveUserChart>
-        <RevenueChart></RevenueChart>
-        <AppDownloads></AppDownloads>
-      </Tab>
-      <Tab eventKey="cloudwatch" title="Cloudwatch"></Tab>
-      <Tab eventKey="optimization" title="Optimization"></Tab>
-    </Tabs>
+    <div class="tabs">
+      <Tabs
+        defaultActiveKey="overview"
+        id="uncontrolled-tab-example"
+        className="mb-3 tab "
+      >
+        <Tab eventKey="overview" title="Overview" className="tab-item">
+          <ActiveUserChart></ActiveUserChart>
+          <RevenueChart></RevenueChart>
+          <AppDownloads></AppDownloads>
+        </Tab>
+        <Tab eventKey="cloudwatch" title="Cloudwatch"></Tab>
+        <Tab eventKey="optimization" title="Optimization"></Tab>
+      </Tabs>
+    </div>
   );
 }
