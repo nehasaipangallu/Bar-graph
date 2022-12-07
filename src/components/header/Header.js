465ff4591c903afe6cb4baedabc00934744af6a7
nehasaipangallu <52908091+nehasaipangallu@users.noreply.github.com>
2022-12-07 13:49:41 +0530

    style improvements

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import './Header.css'
+import './Header.css';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 
-
-export  default function Header(){
-  return(
-    <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home" className="nav-text">Dashboard</Navbar.Brand>
-          <Nav className="me-auto">
-          </Nav>
-        </Container>
-      </Navbar>
-  )
+export default function Header() {
+  return (
+    <Navbar bg="dark" variant="dark" className="nav-bar-bg">
+      <Container>
+        <Navbar.Brand href="#home" className="nav-text">
+          Dashboard
+        </Navbar.Brand>
+        <Nav className="me-auto"></Nav>
+      </Container>
+    </Navbar>
+  );
 }
