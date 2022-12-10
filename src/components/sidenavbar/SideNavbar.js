d343fceabb9c7c7f57026d49165cc6813a8741c4
nehasaipangallu <52908091+nehasaipangallu@users.noreply.github.com>
2022-12-10 22:12:53 +0530

    style enhacnement

--- src/components/sidenavbar/SideNavbar.js
+++ src/components/sidenavbar/SideNavbar.js
@@ -1,41 +1,70 @@
-
 import React from 'react';
-import {CDBSidebar, CDBSidebarContent,  CDBSidebarFooter,  CDBSidebarHeader,  CDBSidebarMenu, CDBSidebarMenuItem} from 'cdbreact'
-import { NavLink } from 'react-router-dom'
+import {
+  CDBSidebar,
+  CDBSidebarContent,
+  CDBSidebarFooter,
+  CDBSidebarHeader,
+  CDBSidebarMenu,
+  CDBSidebarMenuItem,
+} from 'cdbreact';
+import { NavLink } from 'react-router-dom';
 
-export default function SideNavbar(){
-  return(
-  <div>
- <div style={{ display: 'flex', height: '100vh', overflow: 'scroll initial' }}>
-      <CDBSidebar textColor="#fff" backgroundColor="#333">
-        <CDBSidebarHeader prefix={<i className="fa fa-bars fa-large"></i>}>
-          <a href="/" className="text-decoration-none" style={{ color: 'inherit' }}>
-            Sidebar
-          </a>
-        </CDBSidebarHeader>
+export default function SideNavbar() {
+  return (
+    <div>
+      <div
+        style={{
+          display: 'flex',
+          height: 'Calc(100vh - 47px)',
+          overflow: 'scroll initial',
+        }}
+      >
+        <CDBSidebar textColor="#fff" backgroundColor="#333">
+          <CDBSidebarHeader prefix={<i className="fa fa-bars fa-large"></i>}>
+            <a
+              href="/"
+              className="text-decoration-none"
+              style={{ color: 'inherit' }}
+            >
+              Sidebar
+            </a>
+          </CDBSidebarHeader>
 
-        <CDBSidebarContent className="sidebar-content">
-          <CDBSidebarMenu>
-            <NavLink exact to="/" activeClassName="activeClicked">
-              <CDBSidebarMenuItem icon="columns">Dashboard</CDBSidebarMenuItem>
-            </NavLink>
-            <NavLink exact to="/tables" activeClassName="activeClicked">
-              <CDBSidebarMenuItem icon="table">Tables</CDBSidebarMenuItem>
-            </NavLink>
-            <NavLink exact to="/profile" activeClassName="activeClicked">
-              <CDBSidebarMenuItem icon="user">Profile page</CDBSidebarMenuItem>
-            </NavLink>
-            <NavLink exact to="/analytics" activeClassName="activeClicked">
-              <CDBSidebarMenuItem icon="chart-line">Analytics</CDBSidebarMenuItem>
-            </NavLink>
+          <CDBSidebarContent className="sidebar-content">
+            <CDBSidebarMenu>
+              <NavLink exact to="/" activeClassName="activeClicked">
+                <CDBSidebarMenuItem icon="columns">
+                  Dashboard
+                </CDBSidebarMenuItem>
+              </NavLink>
+              <NavLink exact to="/tables" activeClassName="activeClicked">
+                <CDBSidebarMenuItem icon="table">Tables</CDBSidebarMenuItem>
+              </NavLink>
+              <NavLink exact to="/profile" activeClassName="activeClicked">
+                <CDBSidebarMenuItem icon="user">
+                  Profile page
+                </CDBSidebarMenuItem>
+              </NavLink>
+              <NavLink exact to="/analytics" activeClassName="activeClicked">
+                <CDBSidebarMenuItem icon="chart-line">
+                  Analytics
+                </CDBSidebarMenuItem>
+              </NavLink>
 
-            <NavLink exact to="/hero404" target="_blank" activeClassName="activeClicked">
-              <CDBSidebarMenuItem icon="exclamation-circle">404 page</CDBSidebarMenuItem>
-            </NavLink>
-          </CDBSidebarMenu>
-        </CDBSidebarContent>
+              <NavLink
+                exact
+                to="/hero404"
+                target="_blank"
+                activeClassName="activeClicked"
+              >
+                <CDBSidebarMenuItem icon="exclamation-circle">
+                  404 page
+                </CDBSidebarMenuItem>
+              </NavLink>
+            </CDBSidebarMenu>
+          </CDBSidebarContent>
 
-        <CDBSidebarFooter style={{ textAlign: 'center' }}>
+          {/* <CDBSidebarFooter style={{ textAlign: 'center' }}>
           <div
             style={{
               padding: '20px 5px',
@@ -43,10 +72,9 @@ export default function SideNavbar(){
           >
             Sidebar Footer
           </div>
-        </CDBSidebarFooter>
-      </CDBSidebar>
-    </div>
+        </CDBSidebarFooter> */}
+        </CDBSidebar>
+      </div>
     </div>
   );
-
 }
