08c17b713ffc94648feb1eb680dce4b601aa546f
nehasaipangallu <52908091+nehasaipangallu@users.noreply.github.com>
2022-12-12 12:52:01 +0530

    added firebase config

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyBXrWY7bbePBkza9_PGVOovpfIiYcWZq20',
+  authDomain: 'soulipie.firebaseapp.com',
+  projectId: 'soulipie',
+  storageBucket: 'soulipie.appspot.com',
+  messagingSenderId: '201876828788',
+  appId: '1:201876828788:web:c19120f57065b2ad7f9721',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
